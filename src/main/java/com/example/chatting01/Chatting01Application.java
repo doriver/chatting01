99df929335906bc8d77e7b837e7be5e1899c2ca4
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2025-01-14 11:29:10 +0900

    채팅 만들어야함

---
+++ src/main/java/com/example/chatting01/Chatting01Application.java
@@ -0,0 +1,13 @@
+package com.example.chatting01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatting01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatting01Application.class, args);
+	}
+
+}
