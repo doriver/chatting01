07e5709459f96797ff3b9789ab82e7264f178581
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2025-01-20 10:18:04 +0900

    화면에 회원목록 나오는거 구현함

--- src/main/java/com/example/chatting01/v01/controller/FirstView01Controller.java
+++ src/main/java/com/example/chatting01/v01/controller/FirstView01Controller.java
@@ -5,21 +5,26 @@ import com.example.chatting01.v01.entity.User01;
 import com.example.chatting01.v01.repository.User01Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/v01")
-public class View01Controller {
-
-    @RequestMapping("/ex01")
-    public String aa() {
-        return "chatting/v01/chatEx01";
-    }
+public class FirstView01Controller {
 
     @Autowired
     private User01Repository user01Repository;
+
+    @RequestMapping("/ex01")
+    public String aa(Model model) {
+        List<User01> userList = user01Repository.findAll();
+        model.addAttribute("userList", userList);
+        return "chatting/v01/chatEx01";
+    }
 
     /*
     프론트에서 form태그 쓰면 브라우저 url이 바뀌어서 redirect해줘야함
