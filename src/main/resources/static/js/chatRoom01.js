090cc2c0a8017c4c588bd7795561dca8acfafac4
DESKTOP-RENTAL\user <tkdduq241@naver.com>
2025-01-24 17:53:41 +0900

    채팅방 만들기 시작

---
+++ src/main/resources/static/js/chatRoom01.js
@@ -0,0 +1,74 @@
+
+
+
+async function fetchChatItems() {
+    const userId = $('#userId').val();
+    const recipientId  = $('#recipientId').val();
+    try {
+        const response = await fetch(`/chatting/getChatItems?userId=${userId}&recipientId=${recipientId}`);
+        if (response.ok) {
+            setTimeout(async() => {
+                const chatItemsByDate = await response.json();
+                updateChatContainer(chatItemsByDate);
+            }, 100 );
+        }
+    } catch (error) {
+        console.error("Failed to fetch messages:", error);
+    }
+}
+
+function updateChatContainer(chatItemsByDate) {
+    const chatContainer = document.getElementById("chatContainer");
+    chatContainer.innerHTML = ""; // 기존 메시지 초기화
+
+    for (const [date, chatItems] of Object.entries(chatItemsByDate)) { // map받았을때 이렇게
+        // 날짜 표시
+        const dateDiv = document.createElement("div");
+        dateDiv.className = "text-center mt-2 mb-3";
+        dateDiv.style.fontSize = "0.7rem";
+        dateDiv.style.backgroundColor = "lightgrey";
+        dateDiv.textContent = date;
+        chatContainer.appendChild(dateDiv);
+
+        // 메시지 표시
+        chatItems.forEach(chat => {
+            const chatItemDiv = document.createElement("div");
+
+            if (chat.isMine === 0) {
+                // 받은 메시지
+                chatItemDiv.innerHTML = `
+                    <div>
+                        <img src=""  width="28" style="border-radius: 30%">
+                        <span style="font-size: 0.6rem;">${chat.friendUname}</span>
+                    </div>
+                    <div class="message received">
+                        <p>${chat.message}</p>
+                        <span style="font-size: 0.6rem;">${chat.timeStr}</span>
+                    </div>
+                `;
+            } else {
+                // 보낸 메시지
+                chatItemDiv.innerHTML = `
+                    <div class="message sent">
+                        <span style="font-size: 0.6rem; margin-right: 3px;">
+                            ${chat.timeStr}
+                        </span>
+                        <p>${chat.message}</p>
+                    </div>
+                `;
+            }
+            chatContainer.appendChild(chatItemDiv);
+        });
+    }
+    // input tag
+    const inputTag = document.createElement("input");
+    inputTag.className = "form-control mt-2";
+    inputTag.type = "text";
+    inputTag.id = "messageInput";
+    inputTag.placeholder = "메시지 입력";
+    inputTag.addEventListener("keydown", handleEnterKey);
+    chatContainer.appendChild(inputTag);
+
+    // 스크롤을 가장 아래로 내리기
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
