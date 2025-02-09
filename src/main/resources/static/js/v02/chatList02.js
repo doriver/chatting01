5ea56036c5ae3ba86c5213a0ae0b4d1d4c9dddf0
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-02-09 18:29:12 +0900

    코드 일부 옮김

--- src/main/resources/static/js/v02/chatList02.js
+++ src/main/resources/static/js/v02/chatList02.js
@@ -5,14 +5,65 @@ async function fetchRoomList() {
         const response = await fetch(`/v02/api/rooms`);
         if (response.ok) {
             const chatterList = await response.json();
-            updateRoomList(chatterList, loginId);
+            // updateRoomListTb(chatterList, loginId);
+            updateRoomListCd(roomList, loginId);
         }
     } catch (error) {
         console.error('Failed to fetch roomList:', error);
     }
 }
 
-function updateRoomList(roomList, loginId) {
+
+function updateRoomListCd(roomList, loginId) {
+    var roomCount = roomList.length;
+    // 한줄에 몇개 넣을건지에 따라 결정되는 row수
+    var rowCount = Math.ceil(roomCount/4);
+
+    // 채팅방 목록 들어가는 div
+    const cardBox = document.querySelector('#chatRoomListBox');
+    cardBox.innerHTML = ''; // 기존 내용을 초기화
+
+    // 채팅방들 들어있는 row들 추가하는 로직
+    for (let i = 0; i < rowCount; i++) {
+        let cardRow = document.createElement('div');
+        cardRow.className = "row";
+
+        // 배열을 4개씩 잘라서, 부분배열들마다 row아래에 col-3으로 넣어주는 로직
+        let partList = roomList.slice(4 * i, 4 * (i + 1));
+        partList.forEach(room => {
+            let roomComponent = document.createElement('div');
+            roomComponent.className = "col-3";
+            roomComponent.innerHTML = `
+                <div class="card mb-2" >
+                    <div class="row">
+                        <div class="col-4">
+                            <img alt="IMG">
+                        </div>
+                        <div class="col-8">
+                            <p>${room.mentor.userName}</p>
+                        </div>
+                    </div>
+                    <h4>${room.roomName}</h4>
+                    <div>${room.createdAt}</div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <span>현재인원/${room.userLimit} </span>
+                        <form action="/api/chatting/participant/${room.id}/${loginId}"
+                            method="post">
+                            <button type="submit"> 단톡 참석 </button>
+                        </form>
+                    </div>
+                </div>
+            `;
+            cardRow.appendChild(roomComponent);
+        });
+
+        // 생성한 row를 박스에 넣는거
+        cardBox.appendChild(cardRow);
+    }
+}
+
+// 레거시 코드
+function updateRoomListTb(roomList, loginId) {
 
     const tableBody = document.querySelector('.table > tbody');
     tableBody.innerHTML = ''; // 기존 내용을 초기화
@@ -33,7 +84,7 @@ function updateRoomList(roomList, loginId) {
                 </span>
             </td>
             <td>
-                <form action="/v02/api/participant/${room.id}/${loginId}" 
+                <form action="/api/chatting/participant/${room.id}/${loginId}"
                     method="post">
                     <button type="submit"> 단톡 참석 </button>
                 </form>
